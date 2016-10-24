8e6e255e07a529b345bfb803d8f8f334776ccca1
Deer <wise_deer@ex.ua>
2016-10-24 17:52:26 +0300

    working demo sketch

--- src/gaugeDemo/gaugeDemo.ino
+++ src/gaugeDemo/gaugeDemo.ino
@@ -1,17 +1,40 @@
 #include <OLED_I2C.h>
 #include <gauge.h>
 
+OLED myOLED(SDA, SCL, 8);
+extern uint8_t SmallFont[];
+
+Canvas canvas(&myOLED);
+/*
 GaugeRound gauge1(64, 32, 32,
                   -10, 10, 2,
                   0,
-                  -60, 330);
+                  -60, 240);
+*/
 
-setup()
+GaugeRound gauge1(64, 32, 32,
+                  -10, 10, 2,
+                  0,
+                  0, 270);
+                  
+void setup()
 {
+   Serial.begin(9600);
   
+   myOLED.begin();
+   myOLED.setFont(SmallFont);
+
+   gauge1.attach(&canvas);
+
+   gauge1.setValue(0);
+   delay(1000);
 }
 
-loop()
+void loop()
 {
-
+  for(int i = -10; i <= 10; ++i)
+  {
+     gauge1.setValue(i);
+     delay(100);
+  }
 }
